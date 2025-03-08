6e1b314438ff8987b342b6c12bede8b4c67574d0
nnreddyshiv <nnreddyshiv@gmail.com>
2025-03-08 12:01:04 -0500

    fixed AB#3

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
 
     <h1>Gen Ai</h1>
-	<h2>Upload your data to get Gen Ai predictions</h2>
+	<h2>Upload your data to get Gen Ai predictions for everyone</h2>
     <img
       alt="Vue logo"
       style="width: 100px; height: 100px"
